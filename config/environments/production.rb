92250c2a7c1a764c59625ad135ed5d3adf121b0c
Josh Nussbaum <joshnuss@gmail.com>
2014-10-29 15:57:54 -0400

    Added config.assets.debug option for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,6 +40,8 @@ Rails.application.configure do
   # Generate digests for assets URLs.
   config.assets.digest = true
 
+  config.assets.debug = true
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
