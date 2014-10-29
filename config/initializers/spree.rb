5045d812b780857107f88e19b082f5032f9c8a0c
Josh Nussbaum <joshnuss@gmail.com>
2014-10-29 15:31:40 -0400

    Removed s3 spree initializer into environment/production

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -9,12 +9,6 @@ Spree.config do |config|
   # Example:
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
-  if Rails.env.production?
-    config.use_s3 = true
-    config.s3_bucket = ENV['S3_BUCKET_NAME']
-    config.s3_access_key = ENV['AWS_ACCESS_KEY']
-    config.s3_secret = ENV['AWS_SECRET']
-  end
 end
 
 Spree.user_class = "Spree::User"
