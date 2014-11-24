d7ae66ef873e0dac1ff404e255deade6c098f1d7
Josh Nussbaum <joshnuss@gmail.com>
2014-11-23 22:32:21 -0500

    Removed public/assets

--- public/assets/spree/backend/images/new-dd2e03346680a7d24a89b34a75098dc2.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  ($('#cancel_link')).click(function(event) {
-    event.preventDefault();
-    ($('.no-objects-found')).show();
-    ($('#new_image_link')).show();
-    return ($('#images')).html('');
-  });
-
-}).call(this);
