4770fb03db64f990a50c8179459746e30458b94a
Josh Nussbaum <joshnuss@gmail.com>
2015-01-24 08:55:19 -0500

    Attempt fixing minification name mangling in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,6 +34,8 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
 
+  config.ng_annotate.process = true
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
